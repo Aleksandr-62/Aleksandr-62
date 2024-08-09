18b023d1c2a31ead5be7a1ee3c965101fb53de23
Александр <shurik-zet@mail.ru>
2024-08-09 22:34:29 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input('Введите строку ')
+print(len(my_string))                             # Кол-во символов в строке
+print(my_string.upper())                          # Вывод строки в верхнем регистре
+print(my_string.lower())                          # Вывод строки в нижнем регистре
+print(my_string.replace(" ",""))      # Вывод строки без пробелов
+print(my_string[0])                               # Вывод первого символа строки
+print(my_string[-1])                              # Вывод последнего символа строки
