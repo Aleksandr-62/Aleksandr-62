8e27866102a9eebd47f467776f41468f714087b8
Александр <shurik-zet@mail.ru>
2024-08-15 18:18:02 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = input('Введите 1-е число:')
+second = input('Ведите 2-е число:')
+third =  input('Ведите 3-е число:')
+if first == second and first == third and second == third:
+    print("3")
+elif first != second and first != third and second != third:
+    print("0")
+else:
+    print ('1')
